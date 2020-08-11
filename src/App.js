2c4fed804a4a2e58c9c1994642a67d3af917b6df
goncmodev <gon.cmo.dev@gmail.com>
2020-08-11 23:54:46 +0200

    Crear state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+
+  // Crear listado de productos
+
+  const [productos, guardarProductos] = useState([
+    { id: 1, nombre: 'Camisa React', precio: 10 },
+    { id: 2, nombre: 'Camisa Vue', precio: 20 },
+    { id: 3, nombre: 'Camisa Angular', precio: 30 },
+    { id: 4, nombre: 'Camisa Node', precio: 40 }
+  ]);
 
   // Fecha
 
