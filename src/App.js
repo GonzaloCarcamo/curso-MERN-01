6fb2ce8cb39bc0399c06dda2d547e569457a88ea
goncmodev <gon.cmo.dev@gmail.com>
2020-08-12 02:03:45 +0200

    Mostrar coleccíón en state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Producto from './components/Producto';
 
 function App() {
 
@@ -25,6 +26,14 @@ function App() {
         titulo='tienda virtual'
       
       />
+
+      <h1>Lista de productos</h1>
+      {productos.map(producto => (
+        <Producto 
+          key={producto.id}
+          producto={producto}
+        />
+      ))}
 
       <Footer 
         fecha={fecha}
---
+++ src/components/Producto.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Producto = ({producto}) => {
+
+    const { nombre, precio, id } = producto
+
+    return (
+        <div>
+            <h2>{nombre}</h2>
+
+        </div>
+    
+        
+        );
+}
+ 
+export default Producto;
